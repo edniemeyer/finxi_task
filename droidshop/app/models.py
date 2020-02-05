71ee0759cd43954951fbb3291234ffb040779da2
eduardo <edniemeyer@gmail.com>
2020-02-05 19:09:39 -0300

    Demand model created

--- droidshop/app/models.py
+++ droidshop/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Demand(models.Model):
+    OPEN = 'OPEN'
+    CLOSED = 'CLOSED'
+    STATUS_CHOICES = [
+        (OPEN, 'Open'),
+        (CLOSED, 'Closed'),
+    ]
+    
+    description = models.TextField()
+    address = models.TextField()
+    info = models.TextField()
+    advertiser = models.ForeignKey('auth.User', related_name='demands', on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default=OPEN,
+    )
