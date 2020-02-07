04e2517d3cc3118ae264387b2f8e6117f5b91a44
eduardo <edniemeyer@gmail.com>
2020-02-07 14:33:46 -0300

    Added Finalize and Delete Demand Tests

--- droidshop/shop/tests/test_views.py
+++ droidshop/shop/tests/test_views.py
@@ -43,25 +43,25 @@ class GetSingleDemandTest(APITestCase):
         self.user = User.objects.create(username='user1')
         client.force_login(user=self.user)
 
-        self.desc1 = Demand.objects.create(
+        self.demand1 = Demand.objects.create(
             description='Desc1', address='Addr1', info='Info1', advertiser=self.user, status=Demand.OPEN)
-        self.desc2 = Demand.objects.create(
+        self.demand2 = Demand.objects.create(
             description='Desc2', address='Addr2', info='Info2', advertiser=self.user, status=Demand.OPEN)
-        self.desc3 = Demand.objects.create(
+        self.demand3 = Demand.objects.create(
             description='Desc3', address='Addr3', info='Info3', advertiser=self.user, status=Demand.OPEN)
 
     def test_get_valid_single_demand(self):
         # get API response
-        url = reverse('demand-detail', kwargs={'pk': self.desc2.pk})
+        url = reverse('demand-detail', kwargs={'pk': self.demand2.pk})
         response = client.get(url, format='json')
         # get data from db
-        demand = Demand.objects.get(pk=self.desc2.pk)
+        demand = Demand.objects.get(pk=self.demand2.pk)
         objects_serializer = DemandSerializer(demand)
         self.assertEqual(response.data, objects_serializer.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_invalid_single_demand(self):
-        url = reverse('demand-detail', kwargs={'pk': -3})
+        url = reverse('demand-detail', kwargs={'pk': -1})
         response = client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
@@ -95,14 +95,14 @@ class UpdateDemandTest(APITestCase):
         self.user = User.objects.create(username='user1')
         client.force_login(user=self.user)
 
-        self.desc1 = Demand.objects.create(
+        self.demand1 = Demand.objects.create(
             description='Desc1', address='Addr1', info='Info1', advertiser=self.user, status=Demand.OPEN)
 
     def test_update_demand(self):
         """
         Ensure we can update a demand object.
         """
-        url = reverse('demand-detail', kwargs={'pk': self.desc1.pk})
+        url = reverse('demand-detail', kwargs={'pk': self.demand1.pk})
         data = {'description': 'descriptionUpdated',
                 'address': 'addressUpdated',
                 'info': 'infoUpdated'}
@@ -115,3 +115,56 @@ class UpdateDemandTest(APITestCase):
         self.assertEqual(Demand.objects.get().status, Demand.OPEN)
         self.assertEqual(Demand.objects.get().advertiser, self.user)
 
+
+class DeleteDemandTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='user1')
+        client.force_login(user=self.user)
+
+        self.demand1 = Demand.objects.create(
+            description='Desc1', address='Addr1', info='Info1', advertiser=self.user, status=Demand.OPEN)
+
+    def test_valid_delete_demand(self):
+        """
+        Ensure we can delete a demand object.
+        """
+        url = reverse('demand-detail', kwargs={'pk': self.demand1.pk})
+        response = client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_invalid_delete_demand(self):
+        """
+        Ensure we cannot delete an invalid demand object.
+        """
+        url = reverse('demand-detail', kwargs={'pk': -1})
+        response = client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+class FinalizeDemandTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='user1')
+        client.force_login(user=self.user)
+
+        self.demand1 = Demand.objects.create(
+            description='Desc1', address='Addr1', info='Info1', advertiser=self.user, status=Demand.OPEN)
+
+    def test_valid_finalize_demand(self):
+        """
+        Ensure we can finalize a demand object.
+        """
+        url = reverse('demand-finalize', kwargs={'pk': self.demand1.pk})
+        response = client.put(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Demand.objects.get().status, Demand.CLOSED)
+
+    def test_invalid_finalize_demand(self):
+        """
+        Ensure we can finalize a demand object.
+        """
+        #close demand first
+        self.test_valid_finalize_demand()
+        #try to close again
+        url = reverse('demand-finalize', kwargs={'pk': self.demand1.pk})
+        response = client.put(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data, {'status': 'Demand has been already closed!'})
